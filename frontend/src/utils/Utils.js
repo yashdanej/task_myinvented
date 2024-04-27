57d3e194b919b7a41a564073ceef25744591a26f
yashdanej <yashdanej2004@gmail.com>
2024-04-27 12:47:55 +0530

    adding ip

--- frontend/src/utils/Utils.js
+++ frontend/src/utils/Utils.js
@@ -13,7 +13,7 @@ export const changeText = (e, set, state,  setParam=false) => {
 
 export const api = async (pathname, method, body, formData=false, includeCredentials = false) => {
     const axiosConfig = {
-        url: `https://myinvented.onrender.com/${pathname}`,
+        url: `http://13.235.16.221/${pathname}`,
         method: method,
     };
     if(body){
